602acd5e8dc674c750a95c270f550e20a6fd51c2
GianlucaBeltran <gianlucabeltran@gmail.com>
2025-03-17 20:08:42 +0100

    add modal with data info

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -1,9 +1,31 @@
-import { ReactElement } from "react";
+import { ReactElement, RefObject, useEffect, useRef } from "react";
 import PageNav from "./PageNav";
 import "./PageLayout.css";
 import InfoSVG from "./svg/InfoSVG";
+import Modal from "./Modal";
+import { useLocation } from "react-router";
 
 function PageLayout({ children }: Readonly<{ children: ReactElement }>) {
+  const location = useLocation().pathname;
+  const modalRef = useRef<HTMLDialogElement>(null);
+
+  function toggleModal() {
+    if (modalRef && modalRef.current) {
+      if (modalRef.current.open) {
+        modalRef.current.close();
+      }
+      modalRef.current.showModal();
+    }
+  }
+
+  useEffect(() => {
+    return () => {
+      if (modalRef && modalRef.current) {
+        modalRef.current.close();
+      }
+    };
+  }, [location]);
+
   return (
     <div className="App">
       <div className="page-header">
@@ -16,11 +38,21 @@ function PageLayout({ children }: Readonly<{ children: ReactElement }>) {
           <h1 className="logo-name">Studyability</h1>
         </div>
         <PageNav />
-        <button>
-          <InfoSVG width={30} height={30} />
+
+        <button
+          onClick={toggleModal}
+          disabled={location === "/about"}
+          style={{ cursor: location === "/about" ? "default" : "pointer" }}
+        >
+          <InfoSVG
+            width={30}
+            height={30}
+            fill={location === "/about" ? "transparent" : "white"}
+          />
         </button>
       </div>
       <div style={{ height: "95%", overflowY: "auto" }}>{children}</div>
+      <Modal modalRef={modalRef} location={location} />
     </div>
   );
 }
