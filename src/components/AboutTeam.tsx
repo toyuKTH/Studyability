4fdee520b8da899ed683894ea79d33081dc03154
GianlucaBeltran <gianlucabeltran@gmail.com>
2025-03-18 15:30:50 +0100

    finish about team section

--- src/components/AboutTeam.tsx
+++ src/components/AboutTeam.tsx
@@ -1,5 +1,60 @@
+import TeamMemberContainer from "./TeamMemberContainer";
+
+export interface ITeamMember {
+  name: string;
+  role: string;
+  contact: string;
+  image: string;
+}
+
+const teamData: ITeamMember[] = [
+  {
+    name: "Bora Berke Sahin",
+    role: "Data Handling Lead",
+    contact: "bbsahin@kth.se",
+    image: "/bora.jpg",
+  },
+  {
+    name: "Diagnosa Fenomena",
+    role: "Frontend Development, User Evaluation",
+    contact: "diagnosa@kth.se",
+    image: "/sasa.jpg",
+  },
+  {
+    name: "Fauzan Helmi Sudaryanto",
+    role: "User Evaluation Lead, Data Handling, Design",
+    contact: "fhsu@kth.se",
+    image: "/fauzan.jpg",
+  },
+  {
+    name: "Gianluca Beltran",
+    role: "Frontend Development Lead, Data Handling",
+    contact: "gbeltran@kth.se",
+    image: "/luca.png",
+  },
+  {
+    name: "Tong Yu",
+    role: "Design Lead, Frontend Development, User Evaluation",
+    contact: "toyu@kth.se",
+    image: "/tong.jpg",
+  },
+];
+
 function AboutTeam() {
-  return <>Lorem Ipsum</>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexWrap: "wrap",
+        gap: "20px",
+        justifyContent: "center",
+      }}
+    >
+      {teamData.map((teamMember) => (
+        <TeamMemberContainer teamMember={teamMember} />
+      ))}
+    </div>
+  );
 }
 
 export default AboutTeam;
